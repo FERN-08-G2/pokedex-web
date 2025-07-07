0bc7029c66aee20f7d142956cc9a9540fdf0ee02
Riitha <myuuta29.00@gmail.com>
2025-07-07 17:51:06 +0700

    feature: detail pokemon page with component  card & evolution

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Router, Routes } from "react-router";
 import Home from "./pages/Home";
+import DetailsPages from "./pages/Detail";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/detail" element={<DetailsPages />} />
         {/* <Route path="/" element={<Home />} /> */} //tambah route yaa untuk
         halaman lain jadi jangan di file yang sudah ada disini di ubah.. jadi
         setiap org buat file baru trus ngoding di file itu aja ya..
